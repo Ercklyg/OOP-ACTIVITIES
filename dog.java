337dc42feb2629b02a99e353b9472482980d5692
Erick Layug <ericklayugschool@gmail.com>
2024-09-20 10:03:19 +0800

    Update dog.java

--- dog.java
+++ dog.java
@@ -1,8 +1,42 @@
-class dog {
+public class Dog{
     String name;
     boolean isMale;
-    int numOfPuppies;
-    dog listOfPuppies;
+    int puppies;
+    String[] doggo = new String[3];
 
+    public String name(){
+        return "name: " + name;
+    }
+
+    public int puppies(){
+        return puppies;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+    
+    public void setNumberOfPuppies(int puppies) {
+        this.puppies = puppies;
+    }
+
+    public String showNumberOfPuppies() {
+        return name + " has " + puppies + " puppies.";
+    }
+
+    public void setPuppies(int puppies) {
+        this.puppies = puppies;
+    }
+
+    public String showPuppies() {
+        if (puppies == 0) {
+            return name + " has no puppies.";
+        } else {
+            return name + " has " + puppies + " puppies.";
+        }
+    }
+
+    public String bark() {
+        return "Woof! Woof!";
+    }
 }
-
